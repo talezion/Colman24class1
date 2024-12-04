9fbd005419edca20601d1b9657b8f989d6722f2a
Tal Zion <talezion@gmail.com>
2024-12-04 16:05:15 +0200

    Adding button listener to main activity

--- app/src/main/java/com/idz/colman24class1/MainActivity.kt
+++ app/src/main/java/com/idz/colman24class1/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.idz.colman24class1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,10 +23,31 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // TODO: 1. Button to navigate to Add Student
+        // TODO: 1. Button to navigate to Add Student ✅
         // TODO: 2. Create a button listener
         // TODO: 3. Create an intent
 
+        val addStudentButton: Button = findViewById(R.id.main_activity_add_student_button)
 
+//        class MyListener: View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                TODO("Not yet implemented")
+//            }
+//        }
+//
+//        val listener = MyListener()
+//        addStudentButton.setOnClickListener(listener)
+
+//        addStudentButton.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
+
+        addStudentButton.setOnClickListener {
+
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
