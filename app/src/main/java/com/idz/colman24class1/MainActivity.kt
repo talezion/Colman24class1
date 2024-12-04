b36a152b8678caee8a396cd17fe9b24729a36def
Tal Zion <talezion@gmail.com>
2024-12-04 15:40:58 +0200

    initial commit

---
+++ app/src/main/java/com/idz/colman24class1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.idz.colman24class1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // TODO: 1. Button to navigate to Add Student
+        // TODO: 2. Create a button listener
+        // TODO: 3. Create an intent
+    }
+}
